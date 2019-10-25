bc6af8ae54c1fb5e9925c874dfd75be0cc9a96bb
parasquid <tristan.gomez@gmail.com>
2019-10-26 00:34:55 +0800

    use gpio 0 instead of 16
    gpio16 is used for waking up from deep sleep;
    it is better to not use this for soft serial.
    
    gpio 0 doesn't have any special functions so we
    can use this instead.
    
    gpio2 is connected to the led and it's always
    useful to have a visual indication of whether
    there is data that is being received

--- hpms_espruino/src/index.js
+++ hpms_espruino/src/index.js
@@ -3,9 +3,9 @@
 import {pack, unpack} from "./utils.js";
 
 // eslint-disable-next-line no-undef
-var s = new Serial();
+let s = new Serial();
 
-s.setup(9600, {tx:16, rx:2});
+s.setup(9600, {tx:0, rx:2});
 
 let command = pack([0x68, 0x01, 0x20, 0x77]);
 s.print(command);
