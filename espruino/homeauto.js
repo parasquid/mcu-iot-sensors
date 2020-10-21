6dfd2b68c0a4d40ef35f265703c196416ed40c52
parasquid <tristan.gomez@gmail.com>
2020-10-21 18:42:40 +0800

    add home automation code

--- espruino/homeauto.js
+++ espruino/homeauto.js
@@ -18,38 +18,18 @@ setInterval(() => {
   }, { interval: 5000 });
 }, 30000);
 
-const changeAdvertising = (btn) => {
+const changeAdvertising = (btn, device) => {
   console.log(`button ${btn.pin} pressed!`);
   pressCount++;
-  switch(btn.pin) {
-    case BTN1.valueOf():
-      NRF.setAdvertising({
-        0xFFFF: [pressCount]
-      }, { interval: 200 });
-      break;
-    case BTN2.valueOf():
-      NRF.setAdvertising({
-        0xFFFE: [pressCount]
-      }, { interval: 200 });
-      break;
-    case BTN3.valueOf():
-      NRF.setAdvertising({
-        0xFFFD: [pressCount]
-      }, { interval: 200 });
-      break;
-    case BTN4.valueOf():
-      NRF.setAdvertising({
-        0xFFFC: [pressCount]
-      }, { interval: 200 });
-      break;
-    default:
-      console.log(`no handler for button ${btn.pin}`);
-  }
+
+  const data = {};
+  data[device] = [pressCount];
+  NRF.setAdvertising(data, { interval: 200 });
 
   digitalPulse(LED, true, 50);
 };
 
-setWatch((btn) => changeAdvertising(btn), BTN1, { repeat: true, edge: 'rising' });
-setWatch((btn) => changeAdvertising(btn), BTN2, { repeat: true, edge: 'rising' });
-setWatch((btn) => changeAdvertising(btn), BTN3, { repeat: true, edge: 'rising' });
-setWatch((btn) => changeAdvertising(btn), BTN4, { repeat: true, edge: 'rising' });
+setWatch((btn) => changeAdvertising(btn, LIGHT), BTN1, { repeat: true, edge: 'rising' });
+setWatch((btn) => changeAdvertising(btn, FAN), BTN2, { repeat: true, edge: 'rising' });
+setWatch((btn) => changeAdvertising(btn, LIGHT_FAN_ON), BTN3, { repeat: true, edge: 'rising' });
+setWatch((btn) => changeAdvertising(btn, LIGHT_FAN_OFF), BTN4, { repeat: true, edge: 'rising' });
